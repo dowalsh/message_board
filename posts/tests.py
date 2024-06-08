e58e2b53918dddcce791297d45bba6eab2ce856d
Dylan Walsh <dylanwitsend@gmail.com>
2024-06-08 18:50:41 +0100

    added tests

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(title='Test Post', content='Test Content')
+
+    def test_post_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.title}'
+        self.assertEqual(expected_object_name, 'Test Post')
+        # test content
+        expected_object_content = f'{post.content}'
+        self.assertEqual(expected_object_content, 'Test Content')
